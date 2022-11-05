da33b17d59fca37c30fdbdba50021464d83651bb
InessaTrushko <inessa.trushko@gmail.com>
2022-11-06 02:59:13 +0300

    Ex5. Парсинг JSON -- задание

--- Ex5 - parsing JSON.py
+++ Ex5 - parsing JSON.py
@@ -2,5 +2,12 @@ import json
 
 text_for_parsing = '{"messages": [{"message": "This is the first message","timestamp": "2021-06-04 16:40:53"},{"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}'
 
-obj = json.loads(text_for_parsing)["messages"][1]
-print(obj["message"])
+key = "messages"
+keyItem = "message"
+
+obj = json.loads(text_for_parsing)[key][1]
+
+if keyItem in obj:
+    print(obj[keyItem])
+else:
+    print(f"Ключа {keyItem} в JSON нет")
